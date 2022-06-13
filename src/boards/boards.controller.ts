e9dddd38fb2743e30505493a3f64caf655dd0f45
junghan <42junghan@gmail.com>
2022-06-13 20:28:28 +0900

    Feat: handler for read CRUD의 Read를 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
@@ -10,4 +10,9 @@ export class BoardsController {
     //     this.boardsService = boardsService;
     // }
     constructor(private boardsService: BoardsService) {}
+
+    @Get('/')
+    getAllBoard() {
+        return this.boardsService.getAllBoards();
+    }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    private boards = []; //다른 컴포넌트에서 사용하는 것을 막아줌
+
+    getAllBoards() {
+        return this.boards;
+    }
+}
